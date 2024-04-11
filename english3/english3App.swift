8cb83cd26f4f293c1aba2581e6bf86c54b354ee3
Ihor Zadyra <zadyra.i@gmail.com>
2024-04-11 15:55:06 +0300

    Initial Commit

---
+++ english3/english3App.swift
@@ -0,0 +1,17 @@
+//
+//  english3App.swift
+//  english3
+//
+//  Created by IZ on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct english3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
